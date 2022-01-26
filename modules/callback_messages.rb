cb2a09182900b25bbaf49860f095d5d5cbf3708c
Anton-Sikorski <Anton.Sikorski@itechart-group.com>
2022-01-26 18:44:30 +0300

    Polishing dialogue. Remove redundunt methods.

--- modules/callback_messages.rb
+++ modules/callback_messages.rb
@@ -10,12 +10,29 @@ class BirthdayBot
         self.callback_message = Listener.message.message
         case Listener.message.data
         when 'birthday'
-          Listener::Response.std_message "Все записи: #{Database.select(Listener.message.from.id)}" # Listener::Response.std_message('Нету записей(')
+          birthday
+          # Listener::Response.std_message('Нету записей(')
+        when 'set_birthday'
+          Listener::StandardMessages.set_birthday
+        end
+      end
+
+      def birthday
+        data = Database.select(Listener.message.from.id)
+        if data.empty?
+          Listener::Response.std_message('Вы пока не добавили ни одной записи!')
+        else
+          answer = String.new
+          data.each do |record|
+            answer += "День рождения #{record[1]} #{record[2]} числа.\n"
+          end
+          Listener::Response.std_message "Все записи: \n#{answer}"
         end
       end
 
       module_function(
         :process,
+        :birthday,
         :callback_message,
         :callback_message=
       )
--- modules/standard_messages.rb
+++ modules/standard_messages.rb
@@ -5,25 +5,21 @@ class BirthdayBot
     # This module assigned to processing all standard messages
     module StandardMessages
       STATES = %w[begin filled_name filled_date confirmed].freeze
+
       def process
+        @message = Listener.message.text
         @user_id = Listener.message.from.id
-        State.save({ user_id: @user_id, name: nil, date: nil, state: STATES[3] }) if State.check_state(@user_id).empty?
+        State.save({ user_id: @user_id, name: nil, date: nil, state: STATES[3] }) if State.check_state(@user_id).nil?
 
         if State.check_state(@user_id)[:state] != STATES[3]
           set_birthday
         else
-          case Listener.message.text
+          case @message
           when '/start'
             start
-          when '/get_birthdays'
-            birthdays
-          when '/set_birthday'
-            set_birthday
           when '/stop'
             Response.std_message 'Пока!'
             exit(1)
-          when '/state'
-            Response.std_message State.check_state(@user_id)
           else
             plug_message
           end
@@ -40,26 +36,46 @@ class BirthdayBot
       end
 
       def set_birthday(user_id = @user_id)
+
+        if @message == '/reset'
+          State.replace({ user_id: user_id, name: nil, date: nil, state: STATES[3] })
+          return Response.std_message 'Отменяем запись'
+        end
+
         pp State.check_state(user_id)
         state = State.check_state(user_id).empty? ? nil : State.check_state(user_id)[:state]
         print "#{state} : state}\n"
 
+
         case state
         when STATES[3]
           State.replace({ user_id: user_id, name: nil, date: nil, state: STATES[0] })
-          Response.std_message 'Сообщи мене имя именинника'
+          Response.std_message 'Сообщи мне имя именинника'
         when STATES[0]
-          State.replace({ user_id: user_id, name: Listener.message.text, date: nil, state: STATES[1] })
+          return Response.std_message 'Invalid name' unless @message.match(/\w+/)
+
+          State.replace({ user_id: user_id, name: @message, date: nil, state: STATES[1] })
           Response.std_message 'Сообщи мене дату в формате дд/мм/гггг'
         when STATES[1]
-          data = { user_id: user_id, name: State.check_state(user_id)[:name], date: Listener.message.text, state: STATES[2] }
+          return Response.std_message 'Invalid date' unless @message.match(%r{\d\d/\d\d/\d\d\d\d})
+
+          data = { user_id: user_id, name: State.check_state(user_id)[:name], date: @message, state: STATES[2] }
           State.replace(data)
-          Response.std_message "Got: #{data}\nSaving?"
+          Response.std_message "Got: name - #{data[:name]}, birth date - #{data[:date]}\nSaving?(y/n)"
         when STATES[2]
-          data = State.check_state(user_id)
-          Database.save(user_id: user_id, name: data[:name], date: data[:date])
-          Response.std_message 'Saved!'
-          State.replace({ user_id: user_id, name: data[:name], date: data[:date], state: STATES[3] })
+          return unless @message == 'y' || @message == 'n'
+
+          case @message
+          when 'y'
+            data = State.check_state(user_id)
+            Database.save(user_id: user_id, name: data[:name], date: data[:date])
+            Response.std_message 'Saved!'
+            State.replace({ user_id: user_id, name: data[:name], date: data[:date], state: STATES[3] })
+          when 'n'
+            State.replace({ user_id: user_id, name: nil, date: nil, state: STATES[0] })
+          else
+            Response.std_message 'Invalid input'
+          end
         else
           Response.std_message 'invalid answer'
         end
